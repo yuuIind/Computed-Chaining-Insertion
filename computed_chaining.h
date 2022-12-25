7916af5506e6e7b35b359257c8e895ee77fab11c
Hakan <70763685+yuuIind@users.noreply.github.com>
2022-12-25 15:46:30 +0300

    Add files via upload

--- computed_chaining.h
+++ computed_chaining.h
@@ -3,31 +3,37 @@
 using namespace std;
 
 class ComputedChaining {
-  
-  private:
-      int size;
-      list<int> *table;
-  
-  public:
-      ComputedChaining(int size) {
-          this->size = size;
-          table = new list<int>[this->size];
-      }
-      int hashFunction(int key);
-      void insert(int key);
-      void print();
+
+private:
+    int size;
+    list<int> *table;
+
+public:
+    ComputedChaining(int size) {
+        this->size = size;
+        table = new list<int>[this->size];
+    }
+    int hashFunction(int key);
+    void insert(int key);
+    void print();
 };
 
 int ComputedChaining::hashFunction(int key) {
-	return key % this->size;
+    return key % this->size;
 }
 
 void ComputedChaining::insert(int key) {
-	int address = hashFunction(key);
+    table[hashFunction(key)].push_back(key);
 }
 
 void ComputedChaining::print() {
-	for(int i = 0; i < key; i++){
-		cout << i << " --> " << i << endl;
-	}
+    for(int i = 0; i < this->size; i++){
+        cout << i;
+        if(!(table[i].empty())){
+            for(list<int>::iterator listIterator = table[i].begin(); listIterator != table[i].end(); listIterator++){
+                cout << " --> " << *listIterator;
+            }
+        }
+        cout << endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
-    int keys[7] = {19,90,55,11,76,65,30}
-  
+    int keys[7] = {19,90,55,11,76,65,30};
+
     ComputedChaining chain(7);
-    
+
     for(int i=0; i<7; i++){
         chain.insert(keys[i]);
     }
-  
+
     chain.print();
-  return 0;
+    return 0;
 }
